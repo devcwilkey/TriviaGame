ab03852209de9c9efe95aab7ece226501ee7eda0
devcwilkey <DevCWilkey@gmail.com>
2019-06-25 00:26:42 -0700

    First Set of Files for Trivia Game and Initial JS Object creation

---
+++ assets/javascript/app.js
@@ -0,0 +1,181 @@
+var that = this;
+var triviaGame = {
+    gameSettings : {
+        Easy : {
+            totalQuestions : 7,
+            secondsPerQuestion : 30,
+            secondsToDisplayAnswer : 7,
+        },
+        Medium : {
+            totalQuestions : 10,
+            secondsPerQuestion : 20,
+            secondsToDisplayAnswer : 5,
+        },
+        Hard : {
+            totalQuestions : 15,
+            secondsPerQuestion : 15,
+            secondsToDisplayAnswer : 3,
+        }
+    },
+    gameVariables : {
+        mode: "",
+    },
+    Trivia : {
+        Easy : {
+            1 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            2 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            3 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            4 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            5 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            6 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            7 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            8 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            9 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            10 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+        },
+        Medium : {
+            1 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            2 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            3 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            4 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            5 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            6 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            7 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            8 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            9 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            10 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+        },
+        Hard : {
+            1 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            2 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            3 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            4 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            5 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            6 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            7 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            8 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            9 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+            10 : {
+                Question : "",
+                Options : ["","","",""],
+                Answer : ""
+            },
+        }
+    }
+}
